1587f0f2183617b6e8ea42744f3eda41a6f3c72b
IsraelDev560 <devisrael560@gmail.com>
2024-06-10 23:14:14 -0300

    Adicionando responsividade, sombra e mudando modo de exibição

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 function submitForm() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
     let show = document.getElementById("result");
 
-    var overlay = document.getElementById("overlay");
+    let overlay = document.getElementById("overlay");
     overlay.style.display = "block";
     show.innerHTML = `<p>Obrigado por entrar em contato, ${name}!</p>
                       <button class="btn-close" onclick="closeResult()">Fechar</button>`
     show.style.display = "block";
 }
 function closeResult() {
-    var overlay = document.getElementById("overlay");
+    let overlay = document.getElementById("overlay");
     overlay.style.display = "none";
 
-    var show = document.getElementById("result");
+    let show = document.getElementById("result");
     show.style.display = "none";
 }
 
