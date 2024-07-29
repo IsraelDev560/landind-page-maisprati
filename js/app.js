830068fb030de17fadc065af2c2aaf62d794209d
IsraelDev560 <devisrael560@gmail.com>
2024-07-29 11:33:34 -0300

    refactor page

--- js/app.js
+++ js/app.js
@@ -29,3 +29,137 @@ window.onload = function () {
         behavior: 'smooth' 
     });
 };
+
+const addToCart = (product, price, image, quantity) => {
+    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    let existingItem = cartItems.find(item => item.product === product);
+
+    if (existingItem) {
+        existingItem.quantity += quantity;
+    } else {
+        cartItems.push({ product, price, image, quantity });
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    displayCartItems();
+}
+
+const openCart = () => {
+    let div = document.getElementById('cartItems');
+    div.style.display = 'block';
+    displayCartItems();
+}
+
+const displayCartItems = () => {
+    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    let div = document.getElementById('cartItems');
+    div.innerText = "";
+    closeButton();
+
+    let totalQuantity = 0;
+    let totalPrice = 0;
+
+    cartItems.forEach((item, index) => {
+        let itemElement = document.createElement('p');
+
+        let itemImage = document.createElement('img');
+        itemImage.src = item.image;
+
+        let itemText = document.createElement('span');
+        itemText.textContent = `${item.product} - `;
+
+        let priceElement = document.createElement('span');
+        priceElement.textContent = `Preço: R$${item.price} `;
+
+        let quantityContainer = document.createElement('span');
+        quantityContainer.className = 'quantity-buttons';
+
+        let decreaseButton = document.createElement('button');
+        decreaseButton.innerText = '-';
+        decreaseButton.className = 'decreaseButton';
+        decreaseButton.onclick = () => updateQuantity(index, -1);
+
+        let itemQuantity = document.createElement('span');
+        itemQuantity.textContent = `${item.quantity}`;
+        itemQuantity.className = 'itemQuantity'
+
+        let increaseButton = document.createElement('button');
+        increaseButton.innerText = '+';
+        increaseButton.className = 'increaseButton';
+        increaseButton.onclick = () => updateQuantity(index, 1);
+
+        quantityContainer.appendChild(decreaseButton);
+        quantityContainer.appendChild(itemQuantity);
+        quantityContainer.appendChild(increaseButton);
+
+        let button = document.createElement('button');
+        button.innerText = 'Remover';
+        button.className = 'deleteButton';
+        button.onclick = () => deleteCartItems(index);
+
+        itemElement.appendChild(itemImage);
+        itemElement.appendChild(itemText);
+        itemElement.appendChild(priceElement);
+        itemElement.appendChild(quantityContainer);
+        itemElement.appendChild(button);
+        div.appendChild(itemElement);
+
+        totalQuantity += item.quantity;
+        totalPrice += item.quantity * item.price;
+    });
+
+    displayCartTotal(totalQuantity, totalPrice);
+}
+
+const updateQuantity = (index, change) => {
+    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    cartItems[index].quantity += change;
+
+    if (cartItems[index].quantity <= 0) {
+        cartItems.splice(index, 1);
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    displayCartItems();
+}
+
+const deleteCartItems = (index) => {
+    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    cartItems.splice(index, 1);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    displayCartItems();
+}
+
+const closeButton = () => {
+    let div = document.getElementById('cartItems');
+    let buttonClose = document.createElement('button');
+    buttonClose.innerText = 'X';
+    buttonClose.className = 'closeButton';
+    buttonClose.onclick = () => closeCart();
+
+    div.appendChild(buttonClose);
+}
+
+const closeCart = () => {
+    let div = document.getElementById('cartItems');
+    div.style.display = 'none';
+}
+
+const displayCartTotal = (totalQuantity, totalPrice) => {
+    let div = document.getElementById('cartItems');
+    let totalDiv = document.createElement('div');
+    totalDiv.id = 'cartTotal';
+    totalDiv.innerText = `Total de itens: ${totalQuantity} - Preço total: R$${totalPrice.toFixed(2)}`;
+    div.appendChild(totalDiv);
+}
+
+const displayOpenCartButton = () => {
+    let openButton = document.createElement('button');
+    openButton.innerText = 'Abrir Carrinho';
+    openButton.className = 'openButton';
+    openButton.onclick = () => openCart();
+
+    document.body.appendChild(openButton);
+}
+
+displayOpenCartButton();
